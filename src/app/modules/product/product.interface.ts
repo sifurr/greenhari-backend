36959af8a3427685dd990c304eba521cb29ab117
Md Saifur Rahman <saifurrshishir@gmail.com>
2024-05-21 20:57:36 +0600

    Create an interface for Product

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,19 @@
+type TVariants = {
+  type: string;
+  value: string;
+};
+
+type TInventory = {
+    quantity: number,
+    inStock: boolean
+}
+
+type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: TVariants[];
+  inventory: TInventory;
+};
